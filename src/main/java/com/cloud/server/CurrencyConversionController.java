7427de2ac02116d22dad2513267d94cfb050eff0
chana <chanakaayesh@gmail.com>
2022-01-20 21:55:59 +0100

    msg

--- src/main/java/com/cloud/server/CurrencyConversionController.java
+++ src/main/java/com/cloud/server/CurrencyConversionController.java
@@ -3,6 +3,7 @@ package com.cloud.server;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,9 +12,11 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
-
 	
-			@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+	@Autowired
+	private CurrencyExchangeProxy proxy;
+	
+	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversion calculateCurrencyConversion(@PathVariable String from,
 			@PathVariable String to,
 			@PathVariable BigDecimal quantity
@@ -33,7 +36,23 @@ public class CurrencyConversionController {
 				from, to, quantity,
 				currencyConversion.getConversionMultiple(),
 				quantity.multiply(currencyConversion.getConversionMultiple()),
-				currencyConversion.getEnvironment());
+				currencyConversion.getEnvironment()+ " " + "rest template" );
 	}
 
+			
+			@GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+			public CurrencyConversion calculateCurrencyConversionFeign(@PathVariable String from,
+					@PathVariable String to,
+					@PathVariable BigDecimal quantity
+					){
+				
+				//return responseEntity value
+				CurrencyConversion currencyConversion=proxy.retrieveExchangeValue(from, to);
+				return new CurrencyConversion(currencyConversion.getId(),
+						from, to, quantity,
+						currencyConversion.getConversionMultiple(),
+						quantity.multiply(currencyConversion.getConversionMultiple()),
+						currencyConversion.getEnvironment()+ " " + "feign" );
+			}
+			
 }
---
+++ src/main/java/com/cloud/server/CurrencyExchangeProxy.java
@@ -0,0 +1,16 @@
+package com.cloud.server;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name="currency-conversion", url="localhost:8000")
+public interface CurrencyExchangeProxy {
+	
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyConversion retrieveExchangeValue(
+			@PathVariable String from,
+			@PathVariable String to
+			);
+
+}
